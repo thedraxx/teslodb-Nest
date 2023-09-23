5d932333344e8f76edbbe82e2595cd10b0030de5
thedraxx <thedraxxgames@gmail.com>
2023-09-23 16:25:00 -0300

    get y delete typeORM

---
+++ src/products/dto/create-product.dto.ts
@@ -0,0 +1,41 @@
+import {
+  IsString,
+  MinLength,
+  IsNumber,
+  IsPositive,
+  IsOptional,
+  IsInt,
+  IsArray,
+  IsIn,
+} from 'class-validator';
+
+export class CreateProductDto {
+  @IsString()
+  @MinLength(3)
+  title: string;
+
+  @IsString()
+  @IsOptional()
+  description?: string;
+
+  @IsNumber()
+  @IsPositive()
+  @IsOptional()
+  price?: number;
+
+  @IsString()
+  @IsOptional()
+  slug?: string;
+
+  @IsInt()
+  @IsPositive()
+  @IsOptional()
+  stock?: number;
+
+  @IsString({ each: true })
+  @IsArray()
+  sizes: string[];
+
+  @IsIn(['men', 'woman', 'kid', 'unisex'])
+  gender: string;
+}
---
+++ src/products/entities/product.entity.ts
@@ -0,0 +1,52 @@
+import { BeforeInsert, Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
+
+@Entity()
+export class Product {
+  @PrimaryGeneratedColumn('uuid')
+  id: string;
+
+  @Column('text', {
+    unique: true,
+  })
+  title: string;
+
+  @Column('float', {
+    default: 0,
+  })
+  price: number;
+
+  @Column({
+    type: 'text',
+    nullable: true,
+  })
+  description: string;
+
+  @Column('text', {
+    unique: true,
+  })
+  slug: string;
+
+  @Column('int', {
+    default: 0,
+  })
+  stock: number;
+
+  @Column('text', {
+    array: true,
+  })
+  sizes: string[];
+
+  @Column('text')
+  gender: string;
+
+  //   tags
+  // images
+
+  @BeforeInsert()
+  checkSlugInsert() {
+    if (!this.slug) {
+      this.slug = this.slug = this.title.toLowerCase().replace(/ /g, '-');
+    }
+    this.slug = this.slug.toLowerCase().replace(/ /g, '-').replaceAll(' ', '-');
+  }
+}
